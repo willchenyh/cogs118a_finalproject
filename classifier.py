f66a5841d65e79028f546b586d33dedddaffcc73
Max Oliver Geislinger <omnipotent@Maxs-MacBook-Air.local>
2017-06-14 17:54:43 -0700

    split and test data

--- classifier.py
+++ classifier.py
@@ -30,30 +30,23 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.mixture import GMM
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-
+from sklearn.model_selection import train_test_split,  KFold
 fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, '..', 'models')
 dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 
 
-def train(args):
+def train(classfier, data,labelsNum,nClasses,):
     print("Loading embeddings.")
-    fname = "{}/labels.csv".format(args.workDir)
-    labels = pd.read_csv(fname, header=None).as_matrix()[:, 1]
-    labels = map(itemgetter(1),
-                 map(os.path.split,
-                     map(os.path.dirname, labels)))  # Get the directory.
-    fname = "{}/reps.csv".format(args.workDir)
-    embeddings = pd.read_csv(fname, header=None).as_matrix()
-    le = LabelEncoder().fit(labels)
-    labelsNum = le.transform(labels)
-    nClasses = len(le.classes_)
+    fname = "{}/labels.csv".format(workDir)
+    labels = data[:,0]
+    embeddings = data[:,1:]
+    labelsNum = labels.tolist()
     print("Training for {} classes.".format(nClasses))
-
-    if args.classifier == 'LinearSvm':
+    if classifier == 'LinearSvm':
         clf = SVC(C=1, kernel='linear', probability=True)
-    elif args.classifier == 'GridSearchSvm':
+    elif classifier == 'GridSearchSvm':
         print("""
         Warning: In our experiences, using a grid search over SVM hyper-parameters only
         gives marginally better performance than a linear SVM with C=1 and
@@ -69,130 +62,96 @@ def train(args):
         clf = GridSearchCV(SVC(C=1, probability=True), param_grid, cv=5)
     # ref:
     # http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#example-classification-plot-classifier-comparison-py
-    elif args.classifier == 'DecisionTree':  # Doesn't work best
+    elif classifier == 'DecisionTree':  # Doesn't work best
         clf = DecisionTreeClassifier(max_depth=20)
+
 
     clf.fit(embeddings, labelsNum)
 
-    fName = "{}/classifier.pkl".format(args.workDir)
+    fName = "{}/classifier.pkl".format(workDir)
     print("Saving classifier to '{}'".format(fName))
     with open(fName, 'w') as f:
         pickle.dump((le, clf), f)
 
-
-def infer(args, multiple=False):
-    with open(args.classifierModel, 'rb') as f:
+"""
+"""
+def infer(X,Y,multiple=False,verbose=True):
+    classifierModel = "{}/classifier.pkl".format(workDir)
+    with open(classifierModel, 'rb') as f:
         if sys.version_info[0] < 3:
                 (le, clf) = pickle.load(f)
         else:
                 (le, clf) = pickle.load(f, encoding='latin1')
 
-    print("\n=== {} ===".format(img))
+
     # TODO Store testing represenations in folder 
-    reps = getRep(img, multiple)
-    if len(reps) > 1:
-        print("List of faces in image from left to right")
-    for r in reps:
-        rep = r[1].reshape(1, -1)
-        bbx = r[0]
+    f_x = clf.predict(X)
+    error = np.sum(Y[:,0] != f_x) / float(len(Y))
 
-        start = time.time()
-        predictions = clf.predict_proba(rep).ravel()
-        maxI = np.argmax(predictions)
-        person = le.inverse_transform(maxI)
-        confidence = predictions[maxI]
+    print "\tTesting error is {}".format(error)
+    return error
+    # reps = getRep(img, multiple)
+    # if len(reps) > 1:
+    #     print("List of faces in image from left to right")
+    # for r in reps:
+    #     rep = r[1].reshape(1, -1)
+    #     bbx = r[0]
 
-        if args.verbose:
-            print("Prediction took {} seconds.".format(time.time() - start))
-        if multiple:
-            print("Predict {} @ x={} with {:.2f} confidence.".format(person.decode('utf-8'), bbx,
-                                                                     confidence))
-        else:
-            print("Predict {} with {:.2f} confidence.".format(person.decode('utf-8'), confidence))
+    #     start = time.time()
+    #     predictions = clf.predict_proba(rep).ravel()
+    #     maxI = np.argmax(predictions)
+    #     person = le.inverse_transform(maxI)
+    #     confidence = predictions[maxI]
 
+    #     if verbose:
+    #         print("Prediction took {} seconds.".format(time.time() - start))
+    #     if multiple:
+    #         print("Predict {} @ x={} with {:.2f} confidence.".format(person.decode('utf-8'), bbx,
+    #                                                                  confidence))
+    #     else:
+    #         print("Predict {} with {:.2f} confidence.".format(person.decode('utf-8'), confidence))
+
+    #     # match prediction with label
+    #     # Sum up 
 
 if __name__ == '__main__':
+    workDir = "./training-embeddings"
+    print("Loading embeddings.")
+    fname = "{}/labels.csv".format(workDir)
+    labels = pd.read_csv(fname, header=None).as_matrix()[:, 0:1]
+    fname = "{}/reps.csv".format(workDir)
+    embeddings = pd.read_csv(fname, header=None).as_matrix()
+    le = LabelEncoder().fit(labels)
+    labelsNum = le.transform(labels)
+    nClasses = len(le.classes_)
 
-    parser = argparse.ArgumentParser()
+    print embeddings.shape
+    print labels.shape, embeddings.shape
+    data = np.append(labels,embeddings,axis=1)
+    Y = data[:,0:1]
+    X = data[:,1:]
 
-    parser.add_argument(
-        '--dlibFacePredictor',
-        type=str,
-        help="Path to dlib's face predictor.",
-        default=os.path.join(
-            dlibModelDir,
-            "shape_predictor_68_face_landmarks.dat"))
-    parser.add_argument(
-        '--networkModel',
-        type=str,
-        help="Path to Torch network model.",
-        default=os.path.join(
-            openfaceModelDir,
-            'nn4.small2.v1.t7'))
-    parser.add_argument('--imgDim', type=int,
-                        help="Default image dimension.", default=96)
-    parser.add_argument('--cuda', action='store_true')
-    parser.add_argument('--verbose', action='store_true')
+    # Split dataset
+    # Train on generated embeddings
+    splits = [.20,.40,.50,.60,.80]
+    for split in splits:
+        print "----------------------------------------------------"
+        print "[{},{}] [Train,Test] Split".format(  int(100-(split*100)),\
+                                                int((split*100)))
+        X_train, X_test, Y_train, Y_test = train_test_split(X,Y,
+                                      test_size=split,random_state=42)
+        classifier = "LinearSvm"
+        data = np.append(Y_train,X_train,axis=1)
+        train(classifier,data,labelsNum,nClasses)
 
-    subparsers = parser.add_subparsers(dest='mode', help="Mode")
-    trainParser = subparsers.add_parser('train',
-                                        help="Train a new classifier.")
-    trainParser.add_argument('--ldaDim', type=int, default=-1)
-    trainParser.add_argument(
-        '--classifier',
-        type=str,
-        choices=[
-            'LinearSvm',
-            'GridSearchSvm',
-            'GMM',
-            'RadialSvm',
-            'DecisionTree',
-            'GaussianNB',
-            'DBN'],
-        help='The type of classifier to use.',
-        default='LinearSvm')
-    trainParser.add_argument(
-        'workDir',
-        type=str,
-        help="The input work directory containing 'reps.csv' and 'labels.csv'. Obtained from aligning a directory with 'align-dlib' and getting the representations with 'batch-represent'.")
+        test_error = infer(X_test,Y_test)
+        print "----------------------------------------------------"
 
-    inferParser = subparsers.add_parser(
-        'infer', help='Predict who an image contains from a trained classifier.')
-    inferParser.add_argument(
-        'classifierModel',
-        type=str,
-        help='The Python pickle representing the classifier. This is NOT the Torch network model, which can be set with --networkModel.')
-    inferParser.add_argument('imgs', type=str, nargs='+',
-                             help="Input image.")
-    inferParser.add_argument('--multi', help="Infer multiple faces in image",
-                             action="store_true")
 
-    args = parser.parse_args()
-    if args.verbose:
-        print("Argument parsing and import libraries took {} seconds.".format(
-            time.time() - start))
 
-    if args.mode == 'infer' and args.classifierModel.endswith(".t7"):
-        raise Exception("""
-Torch network model passed as the classification model,
-which should be a Python pickle (.pkl)
-See the documentation for the distinction between the Torch
-network and classification models:
-        http://cmusatyalab.github.io/openface/demo-3-classifier/
-        http://cmusatyalab.github.io/openface/training-new-models/
-Use `--networkModel` to set a non-standard Torch network model.""")
-    start = time.time()
 
-    align = openface.AlignDlib(args.dlibFacePredictor)
-    net = openface.TorchNeuralNet(args.networkModel, imgDim=args.imgDim,
-                                  cuda=args.cuda)
 
-    if args.verbose:
-        print("Loading the dlib and OpenFace models took {} seconds.".format(
-            time.time() - start))
-        start = time.time()
 
-    if args.mode == 'train':
-        train(args)
-    elif args.mode == 'infer':
-        infer(args, args.multi)
+
+
+    
